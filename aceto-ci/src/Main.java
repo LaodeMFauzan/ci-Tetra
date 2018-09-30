8994bed8e7d7d5eea9f2f63f06fd08f0e2f15ba0
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-09-30 20:58:01 +0700

    added : read txt file,standford-core-nlp

--- aceto-ci/src/Main.java
+++ aceto-ci/src/Main.java
@@ -1,5 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import edu.stanford.nlp.coref.data.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.ie.util.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.semgraph.*;
+import edu.stanford.nlp.trees.*;
+
 public class Main {
+
+    public static void readData(String fileName){
+        String line = "";
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("The first step to the end");
+        readData("data/sample-uc.txt");
     }
 }
