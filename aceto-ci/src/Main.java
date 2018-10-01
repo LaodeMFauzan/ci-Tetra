79c89f646faf097caa4a111df87fbe3d3c4b6847
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-10-01 21:58:23 +0700

    added : code to do tagging text problem : import nlp libraru

--- aceto-ci/src/Main.java
+++ aceto-ci/src/Main.java
@@ -1,27 +1,40 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import edu.stanford.nlp.coref.data.*;
-import edu.stanford.nlp.ling.*;
-import edu.stanford.nlp.ie.util.*;
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.semgraph.*;
-import edu.stanford.nlp.trees.*;
+import java.io.*;
+import java.util.List;
+import edu.stanford.nlp.ling.SentenceUtils;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class Main {
 
-    public static void readData(String fileName){
-        String line = "";
+    public static BufferedReader readData(String fileName){
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            while((line = br.readLine()) != null){
-                System.out.println(line);
-            }
-
+            return br;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    public static void main(String[] args) {
-        readData("data/sample-uc.txt");
+    public static void main(String[] args) throws Exception {
+        MaxentTagger tagger = new MaxentTagger(MaxentTagger.DEFAULT_NLP_GROUP_MODEL_PATH);
+        TokenizerFactory<CoreLabel> ptbTokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(),("untokenizable=noneKeep"));
+
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out,"utf-8"));
+
+        DocumentPreprocessor documentPreprocessor = new DocumentPreprocessor(readData("data/sample-uc.txt"));
+        documentPreprocessor.setTokenizerFactory(ptbTokenizerFactory);
+
+        for (List<HasWord> sentence: documentPreprocessor){
+            List<TaggedWord> tSentence = tagger.tagSentence(sentence);
+
+            pw.println(SentenceUtils.listToString(tSentence,false));
+        }
+        pw.close();
     }
 }
